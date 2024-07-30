94dfcad5d7942bd5a0db03662611e96cd6a19466
Omar01501 <152200495+Omar01501@users.noreply.github.com>
2024-07-30 13:26:53 +0100

    Update compareNumbers.test.js

--- compareNumbers.test.js
+++ compareNumbers.test.js
@@ -2,15 +2,15 @@
 const compareNumbers = require('./compareNumbers');
 
 describe('compareNumbers', () => {
-    test('should return -1 when the first number is smaller', () => {
+    test(' return -1 when the first number is smaller', () => {
         expect(compareNumbers(2, 3)).toBe(-1);
     });
 
-    test('should return 1 when the first number is larger', () => {
+    test(' return 1 when the first number is larger', () => {
         expect(compareNumbers(5, 3)).toBe(1);
     });
 
-    test('should return 0 when both numbers are equal', () => {
+    test(' return 0 when both numbers are equal', () => {
         expect(compareNumbers(4, 4)).toBe(0);
     });
 });
